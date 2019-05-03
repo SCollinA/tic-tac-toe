4ed58428e392fc0c40ef80466e4745859c1c54d7
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-05-03 14:40:37 -0400

    Cleaned up row component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RowComponent } from './row/row.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RowComponent
+    RowComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule
