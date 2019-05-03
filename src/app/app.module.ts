7e76eb1b845f2fc13818398167aee6bc047d1181
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-05-03 14:37:24 -0400

    Created row component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RowComponent } from './row/row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RowComponent
   ],
   imports: [
     BrowserModule
